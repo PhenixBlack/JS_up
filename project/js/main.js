56e455618bec52e870bdbd06b7267322aa1e66bd
PhenixBlack <99963655+PhenixBlack@users.noreply.github.com>
2022-09-18 15:14:34 +0300

    homework

--- project/js/main.js
+++ project/js/main.js
@@ -1,24 +1,46 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <img class="product-photo" src="images/kompyuternaya-texnika.jpg" alt="photo">
-                <h3>${title}</h3>
-                <p>${price}</p>
+class ProductList{
+    constructor(container='.products'){
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+             const item = new ProductItem(product);
+             block.insertAdjacentHTML("beforeend",item.render());
+        }
+    }
+}
+
+class ProductItem{
+    constructor(product,img='images/kompyuternaya-texnika.jpg'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render(){
+           return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+    }
+}
 
-renderPage(products);
+let list = new ProductList();
+
+
 
